515651a9637e563e8c95c748eead1fb5bb77faa0
Keiran Reilly <keiranr@uvic.ca>
2016-06-13 16:50:44 -0700

    AAAAAAAAAA

---
+++ server.js
@@ -0,0 +1,85 @@
+"use strict";
+
+var express    = require("express");
+var bodyParser = require("body-parser");
+
+var Storage = require('./lib/MongoDB');
+
+// run server with: PORT=XXXXX node server.js 
+// to have it bind to a port from the cmd line.
+// or change the port number here...
+var port = process.env.PORT || 30118;
+
+var app = express();
+
+var db = new Storage(null, null, 'timer');
+
+// use the parse to get JSON objects out of the request. 
+app.use(bodyParser.json());
+
+// server static files from the public/ directory.
+app.use(express.static('keiranr'));
+
+/**
+ * Handle a request for task data.
+ */
+app.get("/data", function (req, res) {
+    console.log("GET Request to: /data");
+    
+     db.getAllTasks(function(err, data){
+         if(err){
+            res.status(500).send();
+        }else{
+            console.log(data)
+            res.status(200).json(data);
+        } 
+     });
+    
+});
+
+/**
+ * Adds a task to the data store.
+ */
+app.post("/add", function (req, res) {
+
+    console.log("POST Request to: /add");
+    
+    db.addTask(req.body, function(err){
+        if(err){
+            res.status(500).send();
+        }else{
+            res.status(200).send();
+        }
+    });
+    
+    res.status(200).send();
+});
+
+/**
+ * Removes a task from the data store.
+ */
+app.post("/remove", function (req, res) {
+    
+    console.log("POST Request to: /remove");
+    console.log(req.body);
+
+    db.removeTask(parseInt(req.body.id), function(err){
+        if(err){
+            res.status(500).send();
+        }else{
+            res.status(200).send();
+        }
+    });
+
+});
+
+
+app.listen(port, function () {
+    
+    console.log("Listening on port "+port);
+    
+    db.connect(function(){
+        // some message here....
+    });
+    
+});
